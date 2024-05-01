0841fa484572bb46ab40c4cecc080b390d3e691f
botany <61688218+YoshikiKubotani@users.noreply.github.com>
2024-05-02 00:52:21 +0900

    create-first-cache

--- src/test_1.py
+++ src/test_1.py
@@ -1,13 +1,1 @@
-print("test")
-print("test")
-print("test")
-print("test")
-print("test")
-print("test")
 
-print("This change will use cache in the default branch")
-
-print("test")
-print("test")
-
-print("test")
